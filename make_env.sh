388def3a87745a50759d1538f3b7942cdd9d379f
wielants <wielants@users.noreply.github.com>
2016-06-12 17:58:17 +0200

    first version

---
+++ make_env.sh
@@ -0,0 +1,6 @@
+# adapt the entries in this file according to your environment
+# and source this file with "source make-env.sh"
+
+export PATH=$(pwd)/bin:$PATH
+export DEP_CLASSPATH=classes
+export DEP_ROOTDIR=.dep
